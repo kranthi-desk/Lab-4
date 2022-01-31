22356f4fcce6671f2fd36834989bedce403de10c
bhanu <70092039+bhanuprakash789@users.noreply.github.com>
2022-01-31 06:59:02 -0500

    Add files via upload

---
+++ services/matches.js
@@ -0,0 +1,22 @@
+const db = require('./db');
+const helper = require('../helper');
+const config = require('../config');
+
+async function getMultiple(page = 1) {
+  const offset = helper.getOffset(page, config.listPerPage);
+  const rows = await db.query(
+    'SELECT team1,team2,match_winner,win_type,win_margin,venue_id FROM match OFFSET $1 LIMIT $2', 
+    [offset, config.listPerPage]
+  );
+  const data = helper.emptyOrRows(rows);
+  const meta = {page};
+
+  return {
+    data,
+    meta
+  }
+}
+
+module.exports = {
+  getMultiple
+}
